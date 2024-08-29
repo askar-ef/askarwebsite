7afaa39787e99e48fa2c66ce89544ce603b694a2
askar full belajar <108921033+askar-ef@users.noreply.github.com>
2024-08-29 07:33:56 +0700

    image

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -17,74 +17,74 @@ const Portfolio = () => {
       year: "2024",
       description:
         "PINKAN is a desktop application specifically designed to offer innovative learning solutions for children in grades 1 to 2 who have ADHD, incorporating engaging educational games that utilize advanced machine learning technologies, including computer vision and voice recognition, to create a dynamic and personalized learning environment tailored to each child’s unique needs and abilities.",
-      imageUrl: "https://i.ibb.co/com/c2tSD5x/PINKAN-Portfolio.png",
+      imageUrl: "https://i.ibb.co.com/9y2ksXG/PINKAN-Portfolio.png",
       role: "As Product Designer",
       detail:
         "Ideas Brainstorming, Product Design, Scientific Writing, System Analysis, UI/UX Design",
-      logo: "https://i.ibb.co/com/XDWvzS3/PINKAN-LOGO.png",
+      logo: "https://i.ibb.co.com/XDWvzS3/PINKAN-LOGO.png",
     },
     {
       title: "Hubungkeun",
       year: "2024",
       description:
         "Hubungkeun is a mobile app that uses real-time location data to connect and facilitate collaboration among communities in Indonesia. It allows individuals, groups, and organizations to communicate, network, and coordinate activities based on their proximity, promoting community engagement and fostering partnerships.",
-      imageUrl: "https://i.ibb.co/com/S0rsxgL/Hubungkeun-Portfolio.png",
+      imageUrl: "https://i.ibb.co.com/mt8DBKx/Hubungkeun-Portfolio.png",
       role: "As Product Designer",
       detail:
         "Ideas Brainstorming, Product Design, Scientific Writing, UI/UX Design",
-      logo: "https://i.ibb.co/com/TtWjG3c/HUBUNGKEUN-LOGO.png",
+      logo: "https://i.ibb.co.com/TtWjG3c/HUBUNGKEUN-LOGO.png",
     },
     {
       title: "DUWIT",
       year: "2024",
       description:
         "Duwit is a fintech application focused on facilitating smooth digital payments and the integration of electronic cards, specifically aimed at boarding schools. It collaborates with school canteens and nearby vendors operating within these educational facilities.",
-      imageUrl: "https://i.ibb.co/com/47kjZZF/DUWIT-Portfolio.png",
+      imageUrl: "https://i.ibb.co.com/47kjZZF/DUWIT-Portfolio.png",
       role: "As UI/UX Designer",
       detail: "User Interface and Experience Design",
-      logo: "https://i.ibb.co/com/QYGk6N5/DUWIT-LOGO-FULL.png",
+      logo: "https://i.ibb.co.com/rdTXCQX/DUWIT-LOGO.png",
     },
     {
       title: "SyncPos",
       year: "2024",
       description:
         "SyncPos (Synchronize Posyandu) is a digital system that integrates health records from community health posts using national identification numbers (NIK). It aims to improve efficiency, accuracy, accessibility, and security in managing health data for elderly and toddler patients attending these posts.",
-      imageUrl: "https://i.ibb.co/com/xFPvgR5/Sync-Pos-Portfolio.png",
+      imageUrl: "https://i.ibb.co.com/8YT3xS8/Sync-Pos-Portfolio.png",
       role: "As UI/UX Designer",
       detail: "User Interface and Experience Design",
-      logo: "https://i.ibb.co/com/9WCvpgm/SYNCPOS-LOGO.png",
+      logo: "https://i.ibb.co.com/9WCvpgm/SYNCPOS-LOGO.png",
     },
     {
       title: "Potensia",
       year: "2023",
       description:
         "Potensia is a digital system that integrates health records from community health posts using national identification numbers (NIK). It aims to improve efficiency, accuracy, accessibility, and security in managing health data for elderly and toddler patients attending these posts.",
-      imageUrl: "https://i.ibb.co/com/C5Jq9GK/Potensia-Portfolio.png",
+      imageUrl: "https://i.ibb.co.com/d0bSpY4/Potensia-Portfolio.png",
       role: "As Product Designer",
       detail:
         "Ideas Brainstorming, Scientific Writing, User Interface and Experience Design",
-      logo: "https://i.ibb.co/com/MgGBNsc/POTENSIA-LOGO.png",
+      logo: "https://i.ibb.co.com/MgGBNsc/POTENSIA-LOGO.png",
     },
     {
       title: "BroMo",
       year: "2023",
       description:
         "BroMo (Broiler Monitoring) is a mobile application designed for monitoring the chicken coops of a company in real-time. Users, including barn owners and workers (farmers), can track conditions inside the coops through sensors that measure parameters such as ammonia, temperature, and humidity. Furthermore, the application provides a platform for barn workers to submit daily reports on the coop conditions.",
-      imageUrl: "https://i.ibb.co/com/LktBRhJ/Bro-Mo-Portfolio.png",
+      imageUrl: "https://i.ibb.co.com/f1pZFGn/Bro-Mo-Portfolio.png",
       role: "As Project Manager",
       detail: "Project Planning, Documentation, Communication, Negotiation",
-      logo: "https://i.ibb.co/com/m6B7dhL/BROMO-LOGO.png",
+      logo: "https://i.ibb.co.com/m6B7dhL/BROMO-LOGO.png",
     },
     {
       title: "AyoRancang",
       year: "2023",
       description:
         "AyoRancang is a mobile app that leverages AI for personalized consultations and centralizes information on higher education institutions and job opportunities, which are currently scattered across various platforms. This approach aims to provide users with consolidated and accessible resources for effective career planning and decision-making.",
-      imageUrl: "https://i.ibb.co/com/4KJBwK6/Ayo-Rancang-Portfolio.png",
+      imageUrl: "https://i.ibb.co.com/9cGRmcF/Ayo-Rancang-Portfolio.png",
       role: "As Product Designer",
       detail:
         "Ideas Brainstorming, Scientific Writing, User Interface and Experience Design",
-      logo: "https://i.ibb.co/com/TMgBZf7/AYORANCANG-LOGO.png",
+      logo: "https://i.ibb.co.com/ZzM63jF/AYORANCANG-LOGO.png",
     },
   ];
 
@@ -103,7 +103,7 @@ const Portfolio = () => {
 
       <Box
         as="img"
-        src="https://i.ibb.co/com/Z2fKRrN/Pinkan-View-Desktop.jpg"
+        src="https://i.ibb.co.com/bb6LjV7/Pinkan-View-Desktop.jpg"
         alt="Portfolio Banner"
         borderRadius="40px"
         p={5}
